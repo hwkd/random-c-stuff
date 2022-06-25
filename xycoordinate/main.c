918b4c9a82e0fb8a08a4bb9b19371d0fed94283b
Hyun Kim <justinkimkorea@gmail.com>
2022-06-26 11:31:51 +1200

    clean up code

--- xycoordinate/main.c
+++ xycoordinate/main.c
@@ -3,44 +3,64 @@
 #define WIDTH 5
 #define HEIGHT 5
 
+void matrix_a(int height, int width);
+void matrix_b(int height, int width);
+
 int main()
 {
-  int matrix_a[HEIGHT][WIDTH];
-  int i = 0;
-
-  for (size_t y = 0; y < HEIGHT; y++)
-  {
-    for (size_t x = 0; x < WIDTH; x++)
-    {
-      printf("%d ", i);
-      matrix_a[y][x] = i;
-      i += 1;
-    }
-    printf("\n");
-  }
-
-  int matrix_b[HEIGHT * WIDTH];
-  i = 0;
-  for (size_t y = 0; y < HEIGHT; y++)
-  {
-    for (size_t x = 0; x < WIDTH; x++)
-    {
-      matrix_b[x + y * WIDTH] = i;
-      i += 1;
-    }
-  }
-
-  for (size_t i = 0; i < HEIGHT * WIDTH; i++)
-  {
-    printf("%d ", matrix_b[i]);
-  }
-  printf("\n");
-
-  int y = 2, x = 3;
-  // Value at [3, 2] should be 13
-  printf("%d\n", matrix_a[y][x]);
-  printf("%d\n", matrix_b[x + y * WIDTH]);
-  printf("%s\n", matrix_a[y][x] == matrix_b[x + y * WIDTH] ? "true" : "false");
+  // Should print the same result
+  matrix_a(HEIGHT, WIDTH);
+  matrix_b(HEIGHT, WIDTH);
 
   return 0;
+}
+
+// 2D array
+void matrix_a(int height, int width)
+{
+  int matrix[height][width];
+  int i = 0;
+
+  for (size_t y = 0; y < height; y++)
+  {
+    for (size_t x = 0; x < width; x++)
+    {
+      matrix[y][x] = i;
+      i += 1;
+    }
+  }
+
+  int x = 3, y = 2;
+  // Value at [3, 2] should be 13
+  printf("%d, ", matrix[y][x]);
+
+  x = 4;
+  y = 3;
+  // Value at [4, 3] should be 19
+  printf("%d\n", matrix[y][x]);
+}
+
+// 1D array translated from 2D array [x,y] => x + y * WIDTH
+void matrix_b(int height, int width)
+{
+  int matrix[height * width];
+  int i = 0;
+
+  for (size_t y = 0; y < height; y++)
+  {
+    for (size_t x = 0; x < width; x++)
+    {
+      matrix[x + y * width] = i;
+      i += 1;
+    }
+  }
+
+  int x = 3, y = 2;
+  // Value at [3, 2] should be 13
+  printf("%d, ", matrix[x + y * WIDTH]);
+
+  x = 4;
+  y = 3;
+  // Value at [4, 3] should be 19
+  printf("%d\n", matrix[x + y * WIDTH]);
 }
