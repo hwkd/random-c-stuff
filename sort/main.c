4982c5d4620d29c97037dd61ef6645dbd3986f4a
Hyun Kim <justinkimkorea@gmail.com>
2022-06-26 17:28:17 +1200

    add sort runner function

--- sort/main.c
+++ sort/main.c
@@ -1,21 +1,39 @@
+/**
+ * Important lessons learnt:
+ *   - C supports callbacks
+ *   - Arrays are always passed as reference; pointers are copied
+ *   - bool types aren't primitive types in C; requires <stdbool.h> to be included
+ */
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
+#include <stdbool.h>
 
 #include "insertion_sort.h"
 
 #define LENGTH 12
 #define MAX 30
 
-void printList(int list[], int length, char label[]);
+int *generateList(int length);
 int randomInt(int max);
+void runSortFunc(void (*sort)(int *list, size_t length), size_t length);
+void printList(int list[], int length, char label[]);
+void test(int list[], size_t length);
 
 int main(void)
 {
   // Seed random value generator
   srand(time(NULL));
 
-  int list[LENGTH];
+  runSortFunc(insertion_sort, LENGTH);
+
+  return EXIT_SUCCESS;
+}
+
+int *generateList(int length)
+{
+  int *list = malloc(sizeof(int) * LENGTH);
 
   // Initialise list with random numbers
   for (size_t i = 0; i < LENGTH; i++)
@@ -23,13 +41,23 @@ int main(void)
     list[i] = randomInt(MAX);
   }
 
+  return list;
+}
+
+// Generate random integer between 0 and max
+int randomInt(int max)
+{
+  return rand() % max;
+}
+
+void runSortFunc(void (*sort)(int *list, size_t length), size_t length)
+{
+  int *list = generateList(length);
   printList(list, LENGTH, "origin");
-
-  insertion_sort(list, LENGTH);
-
+  sort(list, LENGTH);
   printList(list, LENGTH, "insertion sort");
-
-  return EXIT_SUCCESS;
+  test(list, LENGTH);
+  free(list);
 }
 
 // Simply print the values of the given list
@@ -43,8 +71,17 @@ void printList(int list[], int length, char label[])
   printf("\n");
 }
 
-// Generate random integer between 0 and max
-int randomInt(int max)
+void test(int list[], size_t length)
 {
-  return rand() % max;
+  bool pass = true;
+  for (size_t i = 1; i < length; i++)
+  {
+    if (list[i] < list[i - 1])
+    {
+      pass = false;
+      break;
+    }
+  }
+
+  printf("result: %s\n", pass ? "pass" : "fail");
 }
