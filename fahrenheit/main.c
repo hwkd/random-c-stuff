47431536c7b66270c61c272500c62daec3cb44ae
Hyun Kim <justinkimkorea@gmail.com>
2022-06-29 09:35:50 +1200

    feat: use floating point numbers

--- fahrenheit/main.c
+++ fahrenheit/main.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void celsiusToFahrenheit(int *output, int lower, int upper, int step);
+void celsiusToFahrenheit(float *output, int lower, int upper, int step);
 
 int main()
 {
   int lower = 0, upper = 300, step = 20;
   // Just assume upper, lower, and step does not end up with a floating number
   int size = ((upper - lower) / step) * 2;
-  int *result = (int *)malloc(sizeof(int) * size);
+  float *result = (float *)malloc(sizeof(float) * size);
   celsiusToFahrenheit(result, lower, upper, step);
 
   for (size_t i = 0; i <= size; i += 2)
   {
-    printf("%3d %6d\n", result[i], result[i + 1]);
+    printf("%3.0f %6.1f\n", result[i], result[i + 1]);
   }
 
   free(result);
@@ -21,17 +21,14 @@ int main()
   return EXIT_SUCCESS;
 }
 
-void celsiusToFahrenheit(int *output, int lower, int upper, int step)
+void celsiusToFahrenheit(float *output, int lower, int upper, int step)
 {
-  int fahr, celsius;
+  float fahr, celsius;
 
   int i = 0;
   for (fahr = lower; fahr <= upper; fahr += step)
   {
-    // It is imporant to note that 5 is multiplied to (fahr - 32) first before
-    // division by 9 because dividing 5 by 9 first would result in a truncated
-    // result of 0, which would then result in the final result of 0.
-    celsius = 5 * (fahr - 32) / 9;
+    celsius = (5.0/9.0) * (fahr - 32);
     output[i] = fahr;
     output[i + 1] = celsius;
     i += 2;
