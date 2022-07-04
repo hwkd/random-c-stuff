ceea128bb86f3d167412a2c2a3be11b7ef7d93dc
Hyun Kim <justinkimkorea@gmail.com>
2022-07-05 09:54:40 +1200

    revise powerfn

--- powerfn/main.c
+++ powerfn/main.c
@@ -1,27 +1,27 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int power(int base, int exponent);
+int power(int base, int exp);
 
 int main(void)
 {
-  printf("2^0: %d\n", power(2, 0));
-  printf("2^1: %d\n", power(2, 1));
-  printf("2^2: %d\n", power(2, 2));
-  printf("2^3: %d\n", power(2, 3));
-  printf("2^4: %d\n", power(2, 4));
-  printf("2^5: %d\n", power(2, 5));
+  // Test
+  printf("2^0: %s\n", power(2, 0) == 1 ? "Pass" : "Fail");
+  printf("2^1: %s\n", power(2, 1) == 2 ? "Pass" : "Fail");
+  printf("2^2: %s\n", power(2, 2) == 4 ? "Pass" : "Fail");
+  printf("2^3: %s\n", power(2, 3) == 8 ? "Pass" : "Fail");
+  printf("2^4: %s\n", power(2, 4) == 16 ? "Pass" : "Fail");
+  printf("2^5: %s\n", power(2, 5) == 32 ? "Pass" : "Fail");
 
   return EXIT_SUCCESS;
 }
 
-int power(int base, int exponent)
+int power(int base, int exp)
 {
-  int result;
-
-  for (result = 1; exponent > 0; exponent--)
+  int x;
+  for (x = 1; exp > 0; exp--)
   {
-    result = result * base;
+    x = x * base;
   }
-  return result;
+  return x;
 }
